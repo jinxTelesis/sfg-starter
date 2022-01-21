5e28fd213f95710b3fd15b5066d812c8eb0fb736
dremo <jinxtelesis>
2022-01-20 19:33:36 -0500

    init

---
+++ src/main/java/com/dre/sfgstarter/SfgStarterApplication.java
@@ -0,0 +1,13 @@
+package com.dre.sfgstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dre/sfgstarter/SfgStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dre.sfgstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
